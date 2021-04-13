260b07b052ea701eb25dfbcb7b4a41165ace3ad8
maikully <zeebli123@gmail.com>
2021-04-12 21:24:31 -0400

    add random id if null

--- src/main/java/edu/brown/cs/student/spothouse/LobbyGUI.java
+++ src/main/java/edu/brown/cs/student/spothouse/LobbyGUI.java
@@ -4,11 +4,16 @@ import spark.*;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 public class LobbyGUI implements TemplateViewRoute, Route {
     @Override
     public ModelAndView handle(Request request, Response response) throws Exception {
         String id = request.params(":lobbyID");
+        if (id.isEmpty()) {
+            Random rand = new Random();
+            id = Integer.toString(rand.nextInt(9999));
+        }
         System.out.println(id);
         Map<String, String> lobby = new HashMap<>();
         lobby.put("content", id);
